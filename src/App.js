2f8a7da7769f4574d56820dddaf4a35421cf477b
XamSP <max0720x@gmail.com>
2018-11-06 23:46:54 -0500

    finish

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Home from './components/Home';
 import About from './components/About';
 import Navbar from './components/Navbar';
+import {projects as Projects} from './components/Projects';
+import ProjectDetails from './components/ProjectDetails';
 import { Switch, Route } from 'react-router-dom';
 
 class App extends Component {
@@ -13,6 +15,8 @@ class App extends Component {
         <Switch>
           <Route exact path='/' component={Home}/>
           <Route path='/about' component={About}/>
+          <Route exact path='/projects' component={Projects}/>
+          <Route exact path="/projects/:id" component={ProjectDetails} />
         </Switch>
       </div>
     );
